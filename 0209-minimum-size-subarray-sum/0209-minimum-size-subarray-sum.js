f974f03937eaa6ae917428a4933677267f393dc4
Abdul Rahim Dudekula <abdulrahimcc4516@gmail.com>
2026-07-01 12:06:51 +0530

    Time: 5 ms (14.05%), Space: 58.8 MB (40.95%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function (target, nums) {
+let minLength = Infinity;
+  let start = 0,
+    end = 0,
+    size = 1;
+  let sum = nums[start];
+  while (end < nums.length && start < nums.length) {
+    if (sum >= target) {
+      minLength = Math.min(size, minLength);
+      sum -= nums[start++];
+      size--;
+    } else {
+      size++;
+      sum += nums[++end];
+    }
+  }
+  return minLength === Infinity ? 0 : minLength;
+};
